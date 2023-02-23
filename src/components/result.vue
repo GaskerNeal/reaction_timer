fa2557b4d1560b43b2964cf1ae4a1e61997b8a6a
GaskerNeal <myomyintsoe2017@gmail.com>
2023-02-23 11:35:27 +0630

    first commit

---
+++ src/components/result.vue
@@ -0,0 +1,24 @@
+<template>
+  <p>Result is {{score}}ms</p>
+  <div v-if="score<300">
+    <p>You are so fast.</p>
+  </div>
+  <div v-else-if="score>300 && score<500">
+    <p>You are normal.</p>
+  </div>
+  <div v-else>
+    <p>You are too slow.</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['score'],
+}
+</script>
+
+<style>
+p {
+    color: green;
+}
+</style>
